2aa15c3b292bca5e41f584c12a443ab0d43615b9
KrupDat2020 <mark.krupinsky@gmail.com>
2021-06-02 15:35:42 -0500

    useLocalStorage completed

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,19 +1,15 @@
+import { useState } from "react";
 
-const useLocalStorage = (key, initialValue) => {
-    const [ name, setName ] = useLocalStorage ('name', 'Mark') => {
-        if(localStorage.getItem(key)) {
-            return JSON.parse(localStorage.getItem(key));
-        }
-        localStorage.setItem(key, JSON.stringify(initialValue));
-        return initialValue;
-    };
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
 
-    const setStoredName = (name) => {
-        localStorage.setItem(key, JSON.stringify(name));
-        setStoredName(name);
-    }
+  const setValue = (value) => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
 
-    return [name, setStoredName];
-}
-
-export default useLocalStorage;
+  return [storedValue];
+};
